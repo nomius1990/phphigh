479f0e60c0081170880daa5116e35d4a0b1b4c53
duty <duty@silksoftware.com>
2019-04-27 21:38:57 +0800

    魔术方法 __get __set __call __callStatic

---
+++ magicMethod.php
@@ -0,0 +1,49 @@
+<?php
+
+// php 静态方法  __set  __get  访问不存在或者私有属性的时候能用上
+
+class test{
+
+    private $_name;
+    private $_age;
+
+    //设置默认值  当
+    public function __get($name)
+    {
+        // TODO: Implement __get() method.
+        $this->$name = 'default '.$name;
+        return $this->$name;
+    }
+
+    //设置默认值
+    public function __set($name, $value)
+    {
+        // TODO: Implement __set() method.
+        $this->$name = $value;
+    }
+
+    //防止调用不存在的方法
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement __call() method.
+
+        echo "$name 方法不存在,调用参数是".json_encode($arguments).PHP_EOL;
+    }
+
+    //防止调用不存在的静态方法
+    public static  function __callStatic($name, $arguments)
+    {
+        // TODO: Implement __callStatic() method.
+
+        echo 'you are calling not exist static method '.$name.PHP_EOL;
+    }
+
+
+
+}
+
+$test = new test('2222');
+
+test::nimei();
+
+$test->haha('3333');
