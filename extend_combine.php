4fab9bed1218b3a0795b6eff33f48a14fb7ece00
duty <duty@silksoftware.com>
2019-04-28 13:25:28 +0800

    trait(特征) 在类中使用use 调用 ,这样可以直接拥有 trait 的所有的方法和属性. 如果父类和 trait 包含相同的方法或者属性的话 trait 的优先级高于父类

--- extend_combine.php
+++ extend_combine.php
@@ -9,6 +9,7 @@
  * 类的组合和继承
  * 类和类之间有一种父子关系,子类继承父类的属性和方法称为继承在继承里,子类拥有父类的方法和属性,同时子类也可以有自己的方法和属性   --- 继承
  * 在对象中 某个属性是一个类,并调用它的方法处理问题,这种复用的方式教组合
+ * trait(特征) 使用use 调用 ,这样可以直接拥有 trait 的所有的方法和属性
  *
  */
 
@@ -22,6 +23,10 @@ class p{
 
     public function setName($value){
         $this->name = $value;
+    }
+
+    public function showTest(){
+        echo 'i am from class p';
     }
 
 
@@ -41,6 +46,7 @@ class c{
     public function setAge($value){
         $this->age = $value;
     }
+
 
 }
 
@@ -69,6 +75,25 @@ class com{
 
 }
 
+
+trait traittest{
+    public function showTest(){
+        echo 'i am from trait';
+    }
+
+    public function getAge(){
+
+    }
+}
+
+
+class test extends p{
+    use traittest;
+}
+
 $com = new com();
 $com->setName('i am name');
-echo $com->getName();
+//echo $com->getName();
+
+$test = new test();
+$test->showTest();
